7a66a79db4735048210f790b01f59776da7bc78e
JackP-14 <marcelribotll@gmail.com>
2026-03-24 10:42:31 +0100

    Update Ground.cpp

--- donkeycode/src/entities/Ground.cpp
+++ donkeycode/src/entities/Ground.cpp
@@ -3,12 +3,13 @@
 
 Truss Ramp_0[16];
 
-int adder = 0;
+int adderY = 225;
 void RampSetter(Truss* Ramp, int size) {
 	for (int i = 0; i < size; i++) {
-		Ramp[i].setPos(Ramp[i].TrussPos.x + (float)adder, 225.0f);
-		adder += 16;
-		Ramp[i].truss = LoadTexture("TRUSS.png");
+		Ramp[i].setPos(Ramp[i].TrussPos.x + adderX, adderY);
+		adderX += 16;
+		adderY -= 8;
+		Ramp[i].truss = LoadTexture("sprites/TRUSS.png");
 	}
 }
 
