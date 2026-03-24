94350182c7daa80ac5d6d6e9b8940da3165df569
xdHyfa <nilmuniente@gmail.com>
2026-03-24 10:00:04 +0100

    update fire spawner

--- donkeycode/src/entities/Fire_Spawner.cpp
+++ donkeycode/src/entities/Fire_Spawner.cpp
@@ -7,7 +7,7 @@ Fire Fire2;
 
 void SpawnFire() {
 	if (!Fire1.has_Spawned) {
-		Fire1.texture = LoadTexture("FIREBALL.png");
+		Fire1.texture = LoadTexture("sprites/FIREBALL.png");
 		Fire1.SetPos(100,200);
 		Fire1.has_Spawned = true;
 	}
