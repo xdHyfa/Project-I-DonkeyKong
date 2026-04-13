f667ebba8a30264e4d8e3978354a13c8a28f1d41
Dunge0nraider <dmartinez@xifrado.eu>
2026-04-13 12:28:27 +0200

    Names defined and printed in initial Screen

--- donkeycode/src/scenes/InitialScreen.cpp
+++ donkeycode/src/scenes/InitialScreen.cpp
@@ -4,11 +4,28 @@
 #include "core/constants.h"
 #include "scenes.h"
 
+//DEFINE TEXTURES
 
 Texture LogoGrupo;
-Texture Names;
+Texture Names1;
+Texture Names2;
+Texture Names3;
+Texture Names4;
+Texture TutorName;
+Texture UNI;
+Texture PROJE;
+
+//DEFINE SCALES 
+
 Rectangle LOGO = { 0, 0, 180, 88 };
-Rectangle NAMES = { 85, 2, 46, 32 };
+Rectangle NAME1 = { 85, 20, 46, 32 };
+Rectangle NAME2 = { 105, 20, 46, 32 };
+Rectangle NAME3 = { 125, 20, 46, 32 };
+Rectangle NAME4 = { 145, 20, 46, 32 };
+Rectangle TutorN = { 175, 20, 46, 32 };
+Rectangle UNIV = { 205, 110, 46, 32 };
+Rectangle PROJ = { 230, 110, 46, 32 };
+
 
 
 void runInitialScreen() {
@@ -17,6 +34,14 @@ void runInitialScreen() {
         // TO DO: RUN INITIAL SETTINGS (SET START POSITION, LOAD STUFF...)
         SearchAndSetResourceDir("resources");
         LogoGrupo = LoadTexture("SPRITES/LOGOGRUPO.png");
+        Names1 = LoadTexture("SPRITES/Name L.png");
+        Names2 = LoadTexture("SPRITES/Name M.png");
+        Names3 = LoadTexture("SPRITES/Name N.png");
+        Names4 = LoadTexture("SPRITES/Name D.png");
+        TutorName = LoadTexture("SPRITES/Name Tutor.png");
+        UNI = LoadTexture("SPRITES/UPC - CITM");
+        PROJE = LoadTexture("SPRITES/PROJECT 1");
+
         //Names = LoadTexture("SPRITES/MISSING PNG");
         Scene_Init = true;
 
@@ -27,4 +52,12 @@ void runInitialScreen() {
 
     }
     DrawTextureRec(LogoGrupo, LOGO, { 22.0f, 64.0f }, WHITE);
+
+    DrawTextureRec(Names1, NAME1, { 22.0f, 64.0f }, WHITE);
+    DrawTextureRec(Names2, NAME2, { 22.0f, 64.0f }, WHITE);
+    DrawTextureRec(Names3, NAME3, { 22.0f, 64.0f }, WHITE);
+    DrawTextureRec(Names4, NAME4, { 22.0f, 64.0f }, WHITE);
+    DrawTextureRec(TutorName, TutorN, { 22.0f, 64.0f }, WHITE);
+    DrawTextureRec(UNI, UNIV, { 22.0f, 64.0f }, WHITE);
+    DrawTextureRec(PROJE, PROJ, { 22.0f, 64.0f }, WHITE);
 }
