050e61aa3334d9ecc3296ae7cc980cccf3d800f1
xdHyfa <nilmuniente@gmail.com>
2026-05-11 12:15:31 +0200

    a little update

--- source/Collision Handout - Solution/Project1/Entities/objects.cpp
+++ source/Collision Handout - Solution/Project1/Entities/objects.cpp
@@ -49,8 +49,8 @@ void Interactable::SetObject(int x, int y, Items Tag) {
 	}
 	if (Tag == StarItem) {
 		tag = StarItem;
-		SpriteSelector.width = 20;
-		SpriteSelector.height = 20;
+		SpriteSelector.width = 22;
+		SpriteSelector.height = 22;
 		SpriteSelector.x = 0;
 		SpriteSelector.y = 0;
 		Sprite = LoadTexture("Sprites/Star.png");  // <-- añade Star.png a Sprites/
