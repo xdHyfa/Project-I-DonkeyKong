949e906f3f53c667a5eb93448e7a287b3feaeb8c
JackP-14 <marcelribotll@gmail.com>
2026-05-13 14:55:17 +0200

    DonkeyProgress

--- source/Collision Handout - Solution/Project1/Scenes/DonkeyShop.cpp
+++ source/Collision Handout - Solution/Project1/Scenes/DonkeyShop.cpp
@@ -7,15 +7,24 @@
 using namespace std;
 
 Music DonkeyShop;
+Sound ButtonSound;
 bool ChangeThingy = false;
 int selection = 4; 
 Font Shop_Font;
-Vector2 TitlePos = { 40 , 50};
+Vector2 TitlePos = { 40 , 30};
 Vector2 Option1Pos = {30, 90};
 Vector2 Option2Pos = { 120, 90 };
 Vector2 Option3Pos = { 30, 130 };
 Vector2 Option4Pos = { 120, 130 };
-Vector2 ExitPos = { 80, 160 };
+Vector2 ExitPos = { 95, 220 };
+
+Texture2D DonkoDance;
+Rectangle recDance = { 85, 2, 46, 32 };
+
+bool Customize1 = false;
+bool Customize2 = false;
+bool Customize3 = false;
+bool Customize4 = false;
 
 // 0 --> option 1, 1 --> option 2, 2 --> option 3, 3 --> option 4, 4 --> exit 
 
@@ -27,9 +36,13 @@ void runDonkeyShop(){
        DonkeyShop = LoadMusicStream("Audio/DonkeyShop.wav");
        Shop_Font = LoadFont("Fonts/donkey-kong-arcade-1981.otf");
        DonkeyShop.looping = true;
+       ButtonSound = LoadSound("Audio/Button.wav");
+       DonkoDance = LoadTexture("SPRITES/Donko 2-0.png");
+
        PlayMusicStream(DonkeyShop);
         Scene_Init = true;
     }
+    DrawTextureRec(DonkoDance, recDance, { 88.0f, 165.0f }, WHITE);
     switch (selection) {
     case 0:
         DrawTextEx(Shop_Font, "Option 1", Option1Pos, 10, 0.5f, YELLOW);
@@ -84,18 +97,23 @@ void runDonkeyShop(){
     if (IsKeyPressed(KEY_ENTER)) {
         switch (selection) {
         case 0:
-            
+            PlaySound(ButtonSound);
+            Customize1 = !Customize1;
             break;
         case 1:
-            
+            PlaySound(ButtonSound);
+            Customize2 = !Customize2;
             break;
         case 2:
-            
+            PlaySound(ButtonSound);
+            Customize3 = !Customize3;
             break;
         case 3:
-            
+            PlaySound(ButtonSound);
+            Customize4 = !Customize4;
             break;
         case 4:
+            PlaySound(ButtonSound);
             ChangeThingy = true;
             break;
         }
@@ -104,6 +122,7 @@ void runDonkeyShop(){
     DrawTextEx(Shop_Font,"DONKEY SHOP", TitlePos, 10, 0.5f,  WHITE);
     if (ChangeThingy) {
         ChangeScene();
+        UnloadSound(ButtonSound);
         UnloadMusicStream(DonkeyShop);
 
     }
